3f8ed8a3e37948aee8e37ac9f4aaf3b5a15be7fb
翔太 大河原 <190462@jc-21.jp>
2022-02-14 15:52:48 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTest_DBkadai2
+{
+    public class JetCoaster
+    {
+        public static int ride
+            (int k,int[] g,int r)
+        {
+            {
+                int n = 0;
+                int s = k;
+
+
+
+                for (int j = 0; ; j += 1)
+                {
+                    if (r == 0)
+                    {
+                        break;
+                    }
+
+
+
+                    if (j > g.Length - 1)
+                    {
+                        j = 0;
+                    }
+
+
+
+                    Console.WriteLine("g" + g[j]);
+                    if (g[j] <= s)
+                    {
+                        n += g[j];
+                        s -= g[j];
+                    }
+
+
+
+                    else
+                    {
+                        s = k;
+                        r -= 1;
+
+
+
+                        if (j <= g.Length - 1)
+                        {
+                            j -= 1;
+                        }
+                        else
+                        {
+                            j = g.Length - 1;
+                        }
+                    }
+                }
+                return n;
+            }
+        }
+    }
+}
---
+++ kuriage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTest_DBkadai2
+{
+    public class kuriage
+    {
+        public static int Kuriage10(int j)
+        {
+            if (j == 0)
+            {
+                return 0;
+            }else if (j >= 0){
+                double i = j;
+                j = Convert.ToInt32(Math.Ceiling(i / 10) * 10);
+            }
+            else
+            {
+                double i = j;
+                j = Convert.ToInt32(Math.Floor(i / 10) * 10);
+            }
+            
+            return j;
+        }
+    }
+}
