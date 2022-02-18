55f36282da39423fe9843c89c86de34a01b83502
翔太 大河原 <190462@jc-21.jp>
2022-02-18 14:19:08 +0900

    ジェットコースター修正２

--- JetCoaster.cs
+++ JetCoaster.cs
@@ -10,12 +10,16 @@ namespace AutoTest_DBkadai2
     {
         static int i = 0;
 
-
-
         public static int ride(int k, int[] g, int r)
         {
             i = 0;
             int j = 0;
+            int n = 0;
+
+            if (!g.Any())
+            {
+                return n;
+            }
 
             for (int s = 1; s <= r; s++)
             {
